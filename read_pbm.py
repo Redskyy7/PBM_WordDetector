89be4765e8d55262f55f494e492134af69bf7d78
Rodrigo Alves <74573926+Redskyy7@users.noreply.github.com>
2024-03-21 11:18:46 -0300

    Update read_pbm.py

--- read_pbm.py
+++ read_pbm.py
@@ -1,15 +1,9 @@
 def read_pbm(file_path):
     with open(file_path) as f:
         data = [x for x in f if not x.startswith('#')]
-        largura, altura = data[1].split()
-        # for i in range(altura):
-        #     for j in range(largura):
-
-
-    # dimensions = map(int,data.pop(0).split())
-    # arr = []
-    # col_number = 0
-    # for c in data.pop(0):
-    #     if(col_number > dimensions[0]):
-    #         col_number = 0 
-    return int(largura), int(altura)
+        largura, altura = map(int, data[1].split())
+        imagem = [[0]*largura]*altura
+        for i in range(altura-2):
+            for j in range(largura-2):
+                imagem[i][j] = data[2]
+    print(imagem)
