a6493de0641618edd25e99023d4ace4b5ddf5fcb
Rodrigo Alves <74573926+Redskyy7@users.noreply.github.com>
2024-03-25 12:54:03 -0300

    Set as_gray parameter to True

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Run erode in the image
 # With the result dilate it
 # Split array in many parts and create threads to process each one
+# Remove asgray from the function since all the P1 will be only black and white
 import numpy as np
 import read_pbm
 import morphology
@@ -8,8 +9,8 @@ import morphology
 def main():
     print("Bem-vindo ao PBM_WordDetector!")
     width, height, img = read_pbm.read_pbm("./assets/lorem_s12_c02_noise.pbm")
-    result = morphology._dilation(img, False, 1, np.ones((100, 1), dtype=np.int64))
-    final_result = morphology._erosion(result, False, 1, np.ones((100, 1), dtype=np.int64))
+    result = morphology._dilation(img, True, 1, np.ones((100, 1), dtype=np.int64))
+    final_result = morphology._erosion(result, True, 1, np.ones((100, 1), dtype=np.int64))
     data = [[str(item) for item in row] for row in final_result]
     with open('./results.pbm', 'w') as file:
         file.write('P1\n')
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     main()
 
 
-# lorem_s12_c02_espacos, linhas = 102, palavras = 476 
+# lorem_s12_c02_espacos, linhas = 102, palavras = 476
