6b98803b303022f123defb19b4d2c0f234a2ef0b
rodrigoalvesandrade23@gmail.com <rodrigoalvesandrade23@gmail.com>
2024-03-29 13:14:03 -0300

    fix: all operations fixed and working properly

--- morphology.py
+++ morphology.py
@@ -1,100 +1,66 @@
 import numpy as np
 from itertools import product
-
-def _dilation(img, as_gray, sel):
-    """Interface function to call erosion filter"""
-    return apply_filter(_apply_dilation, img, as_gray, sel)
-
-def _apply_dilation(neighbors, as_gray):
-    """Modifies the current pixel value considering its neighbors
-        and the dilation operation rules."""
-    if not as_gray:
-        if max(neighbors.ravel()) == 0:
-            return 0
-        return 1
-    return max(neighbors.ravel())
-
-def _erosion(img, as_gray, sel):
-    """Interface function to call erosion filter"""
-    return apply_filter(_apply_erosion, img, as_gray, sel)
-
-def _apply_erosion(neighbors, as_gray):
-    """Modifies the current pixel value considering its neighbors
-        and the erosion operation rules."""
-    if not as_gray:
-        if max(neighbors.ravel()) == 1:
-            if _selem_is_contained_in(neighbors):
-                return 1
-        return 0
-    return min(neighbors.ravel())
-
-def _opening(img, as_gray, sel):
-    """Applies the opening operation"""
-    eroded = _erosion(img, as_gray, sel)
-    dilated = _dilation(eroded, as_gray, sel)
-    return dilated
-
-def _closing(img, as_gray, sel):
-    """Applies the closing operation"""
-    dilated = _dilation(img, as_gray, sel)
-    return _erosion(dilated, as_gray, sel)
+from datetime import datetime
 
 
+def dilation (img, sel):
+    height, width = img.shape
+    height_sel, width_sel = sel.shape
+    result = np.zeros((height, width), dtype=np.uint8)
+    
+    # Gets the offset value to use and get the neighbors, increments 1 if the height or width is odd
+    offset_height = height_sel // 2
+    offset_height_aux = offset_height
+    if (height_sel % 2 == 1):
+        offset_height_aux += 1
 
-def _selem_is_contained_in(window):
-    """Returns True if ee is contained in win. Otherwise, returns False"""        
-    #selem_white_idx = np.where(_SELEM.flatten() == 1)[0]
-    #selem_sum = np.sum(_SELEM)
-    #win_sum = np.sum(np.take(window, selem_white_idx))
-    #return True if win_sum == selem_sum else False
+    offset_width = width_sel // 2
+    offset_width_aux = offset_width
+    if (width_sel % 2 == 1):
+        offset_width_aux += 1
 
-    idx_selem = np.where(_SELEM.ravel() == 1)[0]
-    idx_win = np.where(window.ravel() == 1)[0]
-    return set(idx_selem) <= set(idx_win)
+    # Iterate in the img array and checks for the positions of the sel
+    for i in range (offset_height, height - offset_height):
+        for j in range (offset_width, width- offset_width):
+            neighborhood = img[i - offset_height: i + offset_height_aux, j - offset_width: j + offset_width_aux]
+            dilated_value = np.max(neighborhood * sel)
+            result[i,j] = dilated_value
 
-def apply_filter(operation, img, as_gray, sel):
-    """Applies a morphological operator to an image"""
-    global _SELEM
-    _SELEM = sel
-    img = img if as_gray else apply_threshold(img)
-    width, height = img.shape
-    prod = product(range(width), range(height))
-    img_result = np.zeros_like(img)
-    radius = _SELEM.shape[0]
-    pad_img = add_padding(img, radius)
-    for i, j in prod:
-        img_result[i, j] = process_pixel(i, j, operation, as_gray, pad_img)
-    return img
-
-def add_padding(img, radius):
-    width, height = img.shape
-    pad_img_shape = (width + radius - 1, height + radius - 1)
-    pad_img = np.zeros(pad_img_shape, dtype=np.uint8)
-    pad_img[radius-2:(width + radius-2), radius-2:(height + radius-2)] = img
-    return pad_img
-
-def process_pixel(i, j, operation, as_gray, img):
-    radius = _SELEM.shape[0]
-    neighbors = img[i:i+radius, j:j+radius]
-    if as_gray:
-        neighbors = np.delete(neighbors.flatten(), radius+1)
-    return operation(neighbors, as_gray)
-
-def apply_threshold(img, threshold=.5):
-    """Applies the given threshold to an image, converting it into black and white"""
-    result = np.ones_like(img, dtype=np.uint8)
-    result[np.abs(img) <= threshold] = 0
-    result[np.abs(img) > threshold] = 1
+    print(f"Dilation completed successfully at {datetime.now()}")
     return result
 
-_OPS = {
-    'er': _erosion,
-    'di': _dilation,
-}
+def erosion (img, sel):
+    height, width = img.shape
+    height_sel, width_sel = sel.shape
+    result = np.zeros((height, width), dtype=np.uint8)
 
-def morph_filter(operator='er',
-                 img=None,
-                 sel=np.ones((3, 2), dtype=np.uint8),
-                 as_gray=False):
-    """Allows to apply multiple morphological operations over an image"""
-    return _OPS[operator](img, as_gray, sel)
+    # Gets the offset value to use and get the neighbors, increments 1 if the height or width is odd
+    offset_height = height_sel // 2
+    offset_height_aux = offset_height
+    if (height_sel % 2 == 1):
+        offset_height_aux += 1
+
+    offset_width = width_sel // 2
+    offset_width_aux = offset_width
+    if (width_sel % 2 == 1):
+        offset_width_aux += 1
+
+    # Iterate in the img array and checks for the positions of the sel
+    for i in range (offset_height, height - offset_height):
+        for j in range (offset_width, width - offset_width):
+            neighborhood = img[i - offset_height: i+ offset_height_aux, j - offset_width: j + offset_width_aux]
+            eroded_value = np.min(neighborhood * sel)
+            result[i,j] = eroded_value
+
+    print(f"Erosion completed successfully at {datetime.now()}")
+    return result
+
+def opening(img, sel):
+    result = erosion(img, sel)
+    result = dilation(result, sel)
+    return result
+
+def closing(img, sel):
+    result = dilation(img, sel)
+    result = erosion(result, sel)
+    return result
